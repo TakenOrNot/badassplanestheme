50c562443deb0576a3db597f87eef7c5a0001a18
gitgud <xdevzone@live.fr>
2018-05-23 01:39:04 +0200

    Initial commit

---
+++ badass.js
@@ -0,0 +1,58 @@
+// ------------------------------------------------------------------------
+//   Badass Planes Theme for StarMash
+// ------------------------------------------------------------------------
+"use strict";
+
+!function()
+{
+    // Returns the filename part of an AirMash's image URL
+    function getFileName(str)
+    {
+        str = str.substring(str.lastIndexOf('/')+1);
+        if (str.indexOf("?")>-1)
+            str = str.substr(0, str.indexOf("?"));
+        return str;
+    }
+
+    // ------------------------------------------------------------------------
+
+    // Theme Function
+    // This theme inherits from VanillaTheme, so we call VanillaTheme's constructor
+    // when this theme is instantiated.
+    class Christmas2017Theme extends VanillaTheme
+    {
+        //constructor() {
+            //super();
+            //my initialization code goes here, after calling super()
+        //}
+
+        // This method called by StarMash when the game is loading
+        injectTextures(files, textureInfo, flagTextureInfo, spriteInfo, textures) 
+        {
+            for(let i in files)
+            {
+                //files[i] = "//localhost/Christmas/" + getFileName(files[i]);
+                files[i] = "//raw.githubusercontent.com/Molesmalo/AirMashChristmasMod/master/assets/" + getFileName(files[i]);
+            }
+        }
+    }
+
+    // We add some metadata to our theme class
+    $.extend(Christmas2017Theme, {
+        themeName: "Badass Planes Theme",
+        description: "As the name says!!",
+        author: "xplay"
+    });
+
+    // ------------------------------------------------------------------------
+
+    // Register our extension and theme
+    SWAM.registerExtension({
+        name: "Badass Planes Theme Extension",
+        id: "Badass2018",
+        description: "Badass Planes Theme",
+        author: "xplay",
+        version: "1.0",
+        themes: [ Badass2018Theme ]
+    });
+}();
