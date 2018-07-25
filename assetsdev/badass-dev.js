3d65c137bece5dbcb398356cd2936ceb83b44388
gitgud <xdevzone@live.fr>
2018-07-25 18:08:30 +0200

    test flag borders

--- assetsdev/badass-dev.js
+++ assetsdev/badass-dev.js
@@ -127,7 +127,7 @@
         if ($( "#redflag-name" ).lenght() !== 0){
             console.log("flag already out");
         }
-        if ($( "#redflag-name" ).lenght() !== 0) && ($( "#blueflag-name" ).lenght() !== 0){
+        if ($( "#redflag-name" ).lenght() !== 0 && $( "#blueflag-name" ).lenght() !== 0){
             console.log("flag not carried atm");
         };
     };
