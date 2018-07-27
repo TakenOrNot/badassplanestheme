3a6b1bf8c6ffcbafa0fde46fbb688668d9ce34a7
gitgud <xdevzone@live.fr>
2018-07-27 15:14:26 +0200

    new sprites, flag borders extension compatibility, loading screen

--- assets/badass.js
+++ assets/badass.js
@@ -18,6 +18,16 @@
         return str;
     }
 
+    jQuery.fn.justtext = function() {
+  
+        return $(this)	.clone()
+                .children()
+                .remove()
+                .end()
+                .text();
+
+    };
+    
     // ------------------------------------------------------------------------
 
     // Theme Function
@@ -58,10 +68,114 @@
     
     // ------------------------------------------------------------------------
     
+    // Custom look for Moz's Flag Borders 
+    // https://github.com/fabiospampinato/airmash-swam-extensions/blob/master/extensions/flag_borders.js
+    /* INIT */
+
+    function init () {
+
+        // initHTML ();
+        initStyle ();
+        initEvents ();
+
+    }
+    
+    
+    
+    function initStyle () {
+
+        const style = `
+          <style>
+            body{background: black url('https://raw.githubusercontent.com/TakenOrNot/badassplanestheme/master/assets/loading.png') 50% 15% no-repeat;}
+            
+            #flag-border-red-right:not([style*="display: none"]){-webkit-animation: neonredright .5s ease-in-out alternate; animation-iteration-count: 11; background : radial-gradient(ellipse closest-side, rgba(255,17,119, .5) 32%, rgba(255,17,119,0) 67%, rgba(255,17,119,0) 100%); height : 4%; top: -2%; }
+            #flag-border-blue-left:not([style*="display: none"]){-webkit-animation: neonblueleft .5s ease-in-out alternate; animation-iteration-count: 11; background : radial-gradient(ellipse closest-side, rgba(0,212,255,0.5) 32%, rgba(9,9,121,0) 67%, rgba(9,9,121,0) 100%); height : 4%; top: -2%; }
+            #flag-border-blue-right {}
+            #flag-border-red-left {}
+            #flag-border-blue-left, #flag-border-blue-right, #flag-border-red-left, #flag-border-red-right {
+                border-width: 0 0 0 0px;
+            }
+          </style>
+        `;
+
+        // $('head').append ( style );
+        $('body').append ( style );
+
+    }
+    
+    function initEvents () {
+        
+        // SWAM.on ( 'CTF_FlagEvent', onFlagEvent );
+
+        SWAM.on ( 'CTF_MatchStarted', onMatchStarted );
+        SWAM.on ( 'CTF_MatchEnded', onMatchEnded );
+        SWAM.on ( 'gamePrep', onGamePrep );
+        // SWAM.on ( 'gameWipe', onGameWipe );
+
+    }
+    
+    SWAM.on ( 'gameLoaded', init );
+    
+    /* EVENTS */
+    
+    
+    
+    function onMatchStarted () {
+        toggleRed ( false );
+        toggleBlue ( false );
+    };
+    function onMatchEnded () {
+        toggleRed ( false );
+        toggleBlue ( false );
+    };
+    function onGamePrep () {
+        // toggleRed ( false );
+        // toggleBlue ( false );
+        var blueflagcheck = $( "#blueflag-name" ).justtext(); 
+        var redflagcheck = $( "#redflag-name" ).justtext();
+        if ( blueflagcheck.lenght > 0){
+            console.log("flag already out " + blueflagcheck);
+        };
+        if ( redflagcheck.lenght > 0){
+            console.log("flag already out " + redflagcheck);
+        }
+        
+    };
+    // function onGameWipe () {
+    //    toggleRed ( false );
+    //    toggleBlue ( false );
+    // };
+     
+    /* API */
+
+  function _toggleElement ( selector, force ) {
+    if ( force === undefined ) {
+      force = !$(selector).is ( ':visible' );
+    }
+    if ( force ) {
+      UI.show ( selector );
+    } else {
+      UI.hide ( selector );
+    }
+  }
+
+  function toggleBlue ( force ) {
+    _toggleElement ( '#flag-border-blue-left', force );
+    _toggleElement ( '#flag-border-blue-right', force );
+  }
+
+  function toggleRed ( force ) {
+    _toggleElement ( '#flag-border-red-left', force );
+    _toggleElement ( '#flag-border-red-right', force );
+  }
+    
+    
+    // ------------------------------------------------------------------------
+    
     // We add some metadata to our theme class
     $.extend(Badass2018Theme, {
         themeName: "Badass Planes Theme",
-        description: "As the name says!!",
+        description: "As the name says !",
         author: "xplay"
     });
 
@@ -71,7 +185,7 @@
     SWAM.registerExtension({
         name: "Badass Planes Theme Extension",
         id: "Badass2018",
-        description: "Badass Planes Theme",
+        description: "As the name says !",
         author: "xplay",
         version: "1.0",
         themes: [ Badass2018Theme ]
