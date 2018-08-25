6576931800267ceca131bb2e5323f3b8e5eb4641
gitgud <xdevzone@live.fr>
2018-08-25 16:30:58 +0200

    test settings

--- assetsdev/badass-dev.js
+++ assetsdev/badass-dev.js
@@ -39,7 +39,7 @@
                 $('body').append ( optionnalFbStyle );
             } 
             else {
-                $('body').remove('#optionnalFbStyle'); 
+                $('#optionnalFbStyle').remove(); 
                 
             }
         }
