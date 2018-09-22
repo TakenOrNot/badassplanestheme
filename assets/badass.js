72338a3cba199c1e8a98fcb9c5517280554396e2
gitgud <xdevzone@live.fr>
2018-09-22 16:24:25 +0200

    update stable 1.0 -> 1.0.2

--- assets/badass.js
+++ assets/badass.js
@@ -6,6 +6,72 @@
 !function()
 {
     
+    // Settings
+    
+    function createSettingsProvider()
+    {
+        // This is the handler that will be executed when new settings are applied
+        function onApply(values)
+        {
+            console.log ("New settings applied: ", values);
+            settings = values;
+            // window.flavor = settings.values1;
+            console.log(settings.values1);
+            paf = settings.values1;
+            // if (flavor == 'default') {
+
+            //    console.log("SETTTTTTINGS")
+
+            // }
+            if (settings.customizeFb === true){
+                console.log("customizeFb !!");
+                const optionnalFbStyle = `
+                    <style id='optionnalFbStyle'>
+                        #flag-border-red-right:not([style*="display: none"]){-webkit-animation: neonredright .5s ease-in-out alternate; animation-iteration-count: 11; background : radial-gradient(ellipse closest-side, rgba(255,17,119, .5) 32%, rgba(255,17,119,0) 67%, rgba(255,17,119,0) 100%); height : 4%; top: -2%; }
+                        #flag-border-blue-left:not([style*="display: none"]){-webkit-animation: neonblueleft .5s ease-in-out alternate; animation-iteration-count: 11; background : radial-gradient(ellipse closest-side, rgba(0,212,255,0.5) 32%, rgba(9,9,121,0) 67%, rgba(9,9,121,0) 100%); height : 4%; top: -2%; }
+                        #flag-border-blue-right {}
+                        #flag-border-red-left {}
+                        #flag-border-blue-left, #flag-border-blue-right, #flag-border-red-left, #flag-border-red-right {
+                            border-width: 0 0 0 0px;
+                        }
+                    </style>
+                `
+                if ( $( "#optionnalFbStyle" ).length ) {
+                    // if element already exist, dont add it again
+                }
+                else {
+                    $('body').append ( optionnalFbStyle );
+                }
+            } 
+            else {
+                $('#optionnalFbStyle').remove(); 
+                
+            }
+        }
+
+        // Default values for the settings
+        let settings = {
+            customizeFb: false,
+            // values1: "default",
+            
+        };
+
+        let sp = new SettingsProvider(settings, onApply);
+    
+        let section = sp.addSection("Look & feel");
+        section.addBoolean("customizeFb", "Customize Moz's Flag Borders apearance");
+
+        // section.addValuesField("values1", "Flavor",
+        // {
+        //    "default": "Default",
+        //    "nuklear": "Nuklear"
+        // });
+        
+        
+        
+        // we return our SettingsProvider instance
+        return sp;
+    }
     
     // ------------------------------------------------------------------------
     
@@ -74,7 +140,7 @@
 
     function init () {
 
-        // initHTML ();
+        initHTML ();
         initStyle ();
         initEvents ();
 
@@ -96,6 +162,16 @@
             #flag-border-blue-left, #flag-border-blue-right, #flag-border-red-left, #flag-border-red-right {
                 border-width: 0 0 0 0px;
             }
+            #badassnews {
+                position: absolute;
+                top: 10px;
+                background: black;
+                margin-left: 50px;
+                width: 300px;
+                font-size : 11px;
+                color: lime;
+                border-radius: 10px;
+            }
             #msg-destroyed, #msg-default, .message .playerbig, .message .player {
                 font-size: 25px;
                 padding: 0px 10px 0px 50px;
@@ -112,6 +188,15 @@
         $('head').append ( headstyle );
         $('body').append ( style );
 
+    }
+    
+    function initHTML () {
+        if ( $( "#badassnews" ).length ) {
+                    // if element already exist, dont add it again
+        }
+        else {
+            $('body').append( "<div id='badassnews'><ul><li>Sep 2 2018 - <a href='https://takenornot.github.io/badassplanestheme/assetsdev/badass-bright.js' target='_blank'>BRIGHT version available !</a> </li><li>Ago 25 2018 - <a href='https://takenornot.github.io/badassplanestheme/assetsdev/badass-beach.js' target='_blank'>BEACH version available !</a> </li><li>Jul 15 2018 - <a href='https://takenornot.github.io/badassplanestheme/assetsdev/badass-nukem.js' target='_blank'>NUKLEAR version available !</a> </li><li>May 23 2018 - <a href='https://takenornot.github.io/badassplanestheme/assets/badass.js' target='_blank'>CLASSIC version available !</a> </li></ul></div>");
+        }
     }
     
     function initEvents () {
@@ -201,7 +286,7 @@
     
     // We add some metadata to our theme class
     $.extend(Badass2018Theme, {
-        themeName: "Badass Planes Theme",
+        themeName: "💀 Badass Planes Theme 💀",
         description: "As the name says !",
         author: "xplay"
     });
@@ -214,7 +299,8 @@
         id: "Badass2018",
         description: "As the name says !",
         author: "xplay",
-        version: "1.0",
+        version: "1.0.2",
+        settingsProvider: createSettingsProvider(),
         themes: [ Badass2018Theme ]
     });
 }();
