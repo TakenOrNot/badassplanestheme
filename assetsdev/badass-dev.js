ff5a8cfa097fd2d348ba1e87df7f2da39f358b49
gitgud <xdevzone@live.fr>
2018-09-22 16:15:38 +0200

    test

--- assetsdev/badass-dev.js
+++ assetsdev/badass-dev.js
@@ -320,7 +320,7 @@
     
     // We add some metadata to our theme class
     $.extend(Badass2018ThemeDev, {
-        themeName: "💀 Badass Planes Theme Dev 💀",
+        themeName: "🔥💀 Badass Planes Theme Dev 💀🔥",
         description: "Experimental",
         author: "xplay"
     });
